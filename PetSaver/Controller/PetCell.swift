a51b50d9fe017a9ec4aa13113c76121f27738340
DevEverton <everton.mercante@gmail.com>
2017-12-19 17:22:44 -0300

    Setup Pet Cell

---
+++ PetSaver/Controller/PetCell.swift
@@ -0,0 +1,19 @@
+//
+//  PetCell.swift
+//  PetSaver
+//
+//  Created by Everton Carneiro on 19/12/17.
+//  Copyright © 2017 Everton. All rights reserved.
+//
+
+import UIKit
+
+class PetCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userLocation: UILabel!
+    @IBOutlet weak var userProfilePicture: UIImageView!
+    @IBOutlet weak var petPicture: UIImageView!
+    @IBOutlet weak var petDescription: UILabel!
+    
+}
--- PetSaver/Controller/PetsVC.swift
+++ PetSaver/Controller/PetsVC.swift
@@ -26,24 +26,44 @@ class PetsVC: UIViewController, UICollectionViewDataSource, UICollectionViewDele
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let petCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+        let petCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! PetCell
         
         petCell.backgroundColor = .white
         petCell.layer.cornerRadius = 8
+        petCell.userName.text = "Carla Lisboa"
+        petCell.userLocation.text = "Parauapebas - PA"
+        petCell.userProfilePicture.image = #imageLiteral(resourceName: "perfilCarla").circle
+        petCell.petPicture.image = #imageLiteral(resourceName: "gato")
+        petCell.petDescription.text = "Gatinhos para adoção, bairro União"
+        
+
         return petCell
     }
 
-
-
 }
-
 
 extension PetsVC: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: view.frame.height - 300)
+        return CGSize(width: view.frame.width, height: view.frame.height - 150)
     }
     
+}
+
+extension UIImage {
+    var circle: UIImage {
+        let square = size.width < size.height ? CGSize(width: size.width, height: size.width) : CGSize(width: size.height, height: size.height)
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: square))
+        imageView.contentMode = UIViewContentMode.scaleAspectFill
+        imageView.image = self
+        imageView.layer.cornerRadius = square.width/2
+        imageView.layer.masksToBounds = true
+        UIGraphicsBeginImageContext(imageView.bounds.size)
+        imageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result!
+    }
 }
 
 
